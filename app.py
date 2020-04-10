5e0eb04da83d99c031dc665c9692878bfa2b04b3
wecassidy <wec.notif.github@gmail.com>
2020-04-10 06:26:14 -0700

    Output over serial instead of stdout

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+
+import serial
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -30,33 +33,46 @@ class ListboxRotator:
         return rotate
 
 
-def start_clicked(widget):
-    label = widget.get_label()
-    if label == "Start":
-        widget.set_label("Stop")
-        print("$START*")
-    else:
-        widget.set_label("Start")
-        print("$STOP*")
+def make_start_clicker(ser):
+    def start_clicked(widget):
+        label = widget.get_label()
+        if label == "Start":
+            widget.set_label("Stop")
+            ser.write(b"$START*\n")
+        else:
+            widget.set_label("Start")
+            ser.write(b"$STOP*\n")
+
+    return start_clicked
 
 
-def send_new_property(spinner):
-    property = Gtk.Buildable.get_name(spinner).upper()
-    if property == "RISE":
-        value = spinner.get_value()
-        print(f"${property},{value:.1f}*")
-    else:
-        value = spinner.get_value_as_int()
-        print(f"${property},{value}*")
+def make_property_sender(ser):
+    def send_new_property(spinner):
+        property = Gtk.Buildable.get_name(spinner).upper()
+        if property == "RISE":
+            value = spinner.get_value()
+            ser.write(f"${property},{value:.1f}*\n".encode("utf-8"))
+        else:
+            value = spinner.get_value_as_int()
+            ser.write(f"${property},{value}*\n".encode("utf-8"))
+
+    return send_new_property
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Must provide serial port as command line argument")
+
+    # Connect to serial
+    ser = serial.Serial(sys.argv[1], baudrate=115200)
+
+    # Build GUI
     builder = Gtk.Builder()
     builder.add_from_file("layout.glade")
     handlers = {
         "onDestroy": Gtk.main_quit,
-        "onStartClick": start_clicked,
-        "propertyChanged": send_new_property,
+        "onStartClick": make_start_clicker(ser),
+        "propertyChanged": make_property_sender(ser),
     }
     builder.connect_signals(handlers)
 
@@ -65,6 +81,7 @@ if __name__ == "__main__":
     window.set_size_request(1920, 1080)
     window.fullscreen()
 
+    # Connect GUI to rotary encoder
     listbox = builder.get_object("control_container")
     rotator = ListboxRotator(listbox)
     r = rotary.RotaryEncoder(
