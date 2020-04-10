30808320ca0410c0e34731d8253de94c3c96d667
wecassidy <wec.notif.github@gmail.com>
2020-04-10 01:17:07 -0700

    Add button switcher, application code

---
+++ app.py
@@ -0,0 +1,25 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+def start_clicked(widget):
+    label = widget.get_label()
+    if label == "Start":
+        widget.set_label("Stop")
+    else:
+        widget.set_label("Start")
+
+
+if __name__ == "__main__":
+    builder = Gtk.Builder()
+    builder.add_from_file("layout.glade")
+    handlers = {"onDestroy": Gtk.main_quit, "onStartClick": start_clicked}
+    builder.connect_signals(handlers)
+
+    window = builder.get_object("main_window")
+    window.show_all()
+    window.fullscreen()
+
+    Gtk.main()
